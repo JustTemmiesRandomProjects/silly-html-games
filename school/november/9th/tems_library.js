fb732387b7eaecf9089ead391d2e5451bae07b8c
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2023-11-22 18:44:14 +0100

    stuff

--- school/november/9th/tems_library.js
+++
@@ -1,9 +0,0 @@
-export function rand_int (offset, multiplier) {
-    return Math.floor(Math.random() * multiplier + offset) 
-}
-
-export function rand_float (offset, multiplier) {
-    return Math.random() * multiplier + offset
-}
-
-// export { rand_float, rand_int }
